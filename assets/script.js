d0ca39b2cf895cc96255d49e93e23d220045ad69
buddy <buddylafluer@gmail.com>
2022-10-11 13:27:39 -0400

    added html and questions to script.js

---
+++ assets/script.js
@@ -0,0 +1,52 @@
+var questionsArray = [
+    {
+        question: "Commonly used data types do NOT include:",
+        answer: "alerts",
+        choices: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers",
+        ],
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with ______.",
+        answer: "parenthesis",
+        choices: [
+            "quotes",
+            "curly brackets",
+            "parentheses",
+            "square brackets",
+        ]
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _______.",
+        answer: "all of the above",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ]
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables",
+        answer: "quotes",
+        choices: [
+            "commas",
+            "curly brackets",
+            "quotes",
+            "parenthesis"
+        ]
+    },
+    {
+        question: "A very useful tool used during development and debugginf for printing content to the debugger is:",
+        answer: "console.log",
+        choices: [
+            "JavaScript",
+            "terminal/bash",
+            "for loops",
+            "console.log"
+        ]
+    }
+];
